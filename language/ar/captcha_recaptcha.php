7d227f1b4483b34036d3dda7ff1287b97c50be99
hubaishan <saeed@hubaishan.com>
2019-11-20 22:41:12 +0300

    Correct spelling

--- language/ar/captcha_recaptcha.php
+++ language/ar/captcha_recaptcha.php
@@ -5,7 +5,7 @@
 *
 * @copyright (c) phpBB Limited <https://www.phpbb.com>
 * @translated 2014 phpBBArabia.com
-* 2017 Updated and mainted by Saeed Hubaishan <http://salafitech.net>
+* 2017 Updated and maintained by Saeed Hubaishan <http://salafitech.net>
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
 * For full copyright and license information, please see

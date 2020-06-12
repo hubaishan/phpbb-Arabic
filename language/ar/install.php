ea70a01466ce524b4fe89f4779e559e1ed08b65f
hubaishan <hubaishan@outlook.com>
2020-06-12 23:45:54 +0300

    Add phpbb-ar.com

--- language/ar/install.php
+++ language/ar/install.php
@@ -4,8 +4,9 @@
  * This file is part of the phpBB Forum Software package.
  *
  * @copyright (c) phpBB Limited <https://www.phpbb.com>
+ * @translated by Saeed Hubaishan <https://phpbb-ar.com>
  * @license GNU General Public License, version 2 (GPL-2.0)
- *
+ * 
  * For full copyright and license information, please see
  * the docs/CREDITS.txt file.
  *
